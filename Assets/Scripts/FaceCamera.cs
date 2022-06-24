e6323dafbd3322e053bfef17c85cb9e34263a963
vanutella <vanutella@gmail.com>
2022-06-24 12:18:31 +0200

    Fixed Billboard Rotation

--- Assets/Scripts/FaceCamera.cs
+++ Assets/Scripts/FaceCamera.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class FaceCamera : MonoBehaviour
 {
     public Transform cameraToLookAt;
+    public Transform screenOrientation;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +14,17 @@ public class FaceCamera : MonoBehaviour
 
     // Update is called once per frame
     void Update()
+    {
+        Debug.Log(screenOrientation.rotation);
+        Vector3 v = screenOrientation.position;
+        Vector3 r = screenOrientation.localRotation.eulerAngles;
+        Debug.Log(r + "  ,  " + v);
+
+        Vector3 newRotation = new Vector3(7.98f, 180, 0);
+        transform.eulerAngles = r;
+    }
+
+    void RotateToCam()
     {
         Vector3 v = cameraToLookAt.transform.position - transform.position;
         v.x = v.z = 0.0f;
